159699e71c9b01f45c5e2186430384768b6b516e
Mark Kevin Adona <adonamarkkevin@gmail.com>
2025-09-01 02:56:33 +0800

    feat: update on screen setting due to gpu upgrade

--- .config/i3/set_workspace.sh
+++ .config/i3/set_workspace.sh
@@ -7,44 +7,34 @@ hostname=$(hostnamectl --static)
 desktop_hostname="archlinux"
 laptop_hostname="archbtw"
 
-# Define monitors
-monitor1="HDMI-0"
-monitor2="DP-1"
+# Define monitors (updated for AMD)
+monitor1="HDMI-A-0"
+monitor2="DisplayPort-1"
 laptop_monitor="eDP-1"
 
 if [ "$hostname" = "$desktop_hostname" ]; then
-    xrandr --output HDMI-0 --primary --left-of DP-1
+    # Arrange displays: DP primary (right), HDMI secondary (left)
+    xrandr --output "$monitor2" --primary --right-of "$monitor1"
 
-    # Desktop configuration
-    i3-msg "workspace 1 output $monitor1"
-    i3-msg "workspace 2 output $monitor1"
-    i3-msg "workspace 3 output $monitor1"
-    i3-msg "workspace 4 output $monitor1"
-    i3-msg "workspace 5 output $monitor1"
-    i3-msg "workspace 6 output $monitor1"
-    i3-msg "workspace 7 output $monitor1"
-    i3-msg "workspace 8 output $monitor1"
-    i3-msg "workspace 9 output $monitor1"
+    # Workspaces on primary (DP-1)
+    i3-msg "workspace 1 output $monitor2"
+    i3-msg "workspace 2 output $monitor2"
+    i3-msg "workspace 3 output $monitor2"
+    i3-msg "workspace 4 output $monitor2"
+    i3-msg "workspace 5 output $monitor2"
+    i3-msg "workspace 6 output $monitor2"
+    i3-msg "workspace 7 output $monitor2"
+    i3-msg "workspace 8 output $monitor2"
+    i3-msg "workspace 9 output $monitor2"
 
-    # Secondary monitor workspaces
-    i3-msg "workspace 11 output $monitor2"
-    i3-msg "workspace 12 output $monitor2"
-    i3-msg "workspace 13 output $monitor2"
-    i3-msg "workspace 14 output $monitor2"
-    i3-msg "workspace 15 output $monitor2"
-    i3-msg "workspace 16 output $monitor2"
-    i3-msg "workspace 17 output $monitor2"
-    i3-msg "workspace 18 output $monitor2"
-    i3-msg "workspace 19 output $monitor2"
-elif [ "$hostname" = "$laptop_hostname" ]; then
-    # Laptop configuration
-    i3-msg "workspace 1 output $laptop_monitor"
-    i3-msg "workspace 2 output $laptop_monitor"
-    i3-msg "workspace 3 output $laptop_monitor"
-    i3-msg "workspace 4 output $laptop_monitor"
-    i3-msg "workspace 5 output $laptop_monitor"
-    i3-msg "workspace 6 output $laptop_monitor"
-    i3-msg "workspace 7 output $laptop_monitor"
-    i3-msg "workspace 8 output $laptop_monitor"
-    i3-msg "workspace 9 output $laptop_monitor"
+    # Secondary monitor workspaces (HDMI)
+    i3-msg "workspace 11 output $monitor1"
+    i3-msg "workspace 12 output $monitor1"
+    i3-msg "workspace 13 output $monitor1"
+    i3-msg "workspace 14 output $monitor1"
+    i3-msg "workspace 15 output $monitor1"
+    i3-msg "workspace 16 output $monitor1"
+    i3-msg "workspace 17 output $monitor1"
+    i3-msg "workspace 18 output $monitor1"
+    i3-msg "workspace 19 output $monitor1"
 fi
